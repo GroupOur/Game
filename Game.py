255ca8f11016dfc766848a253074b3ef41894dab
Jarko Umbrelluck <umbrelluck@gmail.com>
2018-10-17 18:32:39 +0300

    scenario

--- Game.py
+++ Game.py
@@ -5,4 +5,7 @@ def Game():
     fairyTail()
     difficulty = setDifficulty()
     numbers = genNumbers(difficulty)
-    tasks = {"ulam": isUlam}
+    # change easy to different
+    tasks = {"ulam": isUlam, "lucky": None, "easy": None}
+    counter, end, total, goal = 0, 15, 0, 10
+    while counter < end +1 and total<goal
