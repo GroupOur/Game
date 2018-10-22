e1119e87118a07d3ad05dfdbcefb0218536c670f
Jarko Umbrelluck <umbrelluck@gmail.com>
2018-10-22 18:20:34 +0300

    ui

--- valizzka_Game.py
+++ valizzka_Game.py
@@ -4,6 +4,8 @@ import random
 pygame.init()
 pygame.font.init()
 img_lst = ['flask_blue.png', 'flask_red.png']
+
+
 class Flask():
     def __init__(self):
         self.x = random.randint(100, 900)
@@ -12,6 +14,7 @@ class Flask():
         self.move_x = 1 + random.random()
         self.move_y = 1 + random.random()
         self.num = '99'
+
 
 def move(flasks):
     for flask in flasks:
@@ -23,12 +26,13 @@ def move(flasks):
         flask.x += flask.move_x
         flask.y += flask.move_y
         screen.blit(flask.img, (flask.x, flask.y))
-        screen.blit(textsurface,(flask.x + 35, flask.y + 40))
-  
+        screen.blit(textsurface, (flask.x + 35, flask.y + 40))
+
+
 def click(mouse, flasks):
     for flask in flasks:
         if (flask.x + 80 > mouse[0] > flask.x) and\
-        (flask.y + 80  > mouse[1] > flask.y):
+                (flask.y + 80 > mouse[1] > flask.y):
             flasks.remove(flask)
             flasks.append(Flask())
             global score, step
@@ -36,6 +40,7 @@ def click(mouse, flasks):
             step += 1
             print(step)
             break
+
 
 screen = pygame.display.set_mode((1000, 500))
 myfont = pygame.font.SysFont('Comic Sans MS', 20)
@@ -50,20 +55,20 @@ for i in range(col_flasks):
 
 clock = pygame.time.Clock()
 while not done:
-        if (step == 5):
-            screen.fill(white)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    done = True
-        else:
-            mouse = (-1, -1)
-            for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                            done = True
-                    if event.type == pygame.MOUSEBUTTONUP:
-                      click(pygame.mouse.get_pos(), flask_lst)
-        
-            screen.fill(white)
-            move(flask_lst)      
-        pygame.display.flip()
-        clock.tick(60)
+    if (step == 5):
+        screen.fill(white)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+    else:
+        mouse = (-1, -1)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                click(pygame.mouse.get_pos(), flask_lst)
+
+        screen.fill(white)
+        move(flask_lst)
+    pygame.display.flip()
+    clock.tick(60)
