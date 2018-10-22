37ac40702afa5c9c2bd5a97b45a6af54883c33eb
Vizzzka <44239963+Vizzzka@users.noreply.github.com>
2018-10-22 15:53:40 +0300

    oops

--- valizzka_Game.py
+++ valizzka_Game.py
@@ -1,26 +1,69 @@
-from valizzka_helperFunctions import *
+import pygame
+import random
+#import addition
+pygame.init()
+pygame.font.init()
+img_lst = ['flask_blue.png', 'flask_red.png']
+class Flask():
+    def __init__(self):
+        self.x = random.randint(100, 900)
+        self.y = random.randint(100, 400)
+        self.img = pygame.image.load(img_lst[random.randint(0, 1)])
+        self.move_x = 1 + random.random()
+        self.move_y = 1 + random.random()
+        self.num = '99'
 
+def move(flasks):
+    for flask in flasks:
+        textsurface = myfont.render(flask.num, False, white)
+        if (flask.x >= 920) or (flask.x <= 100):
+            flask.move_x *= -1
+        if (flask.y >= 420) or (flask.y <= 0):
+            flask.move_y *= -1
+        flask.x += flask.move_x
+        flask.y += flask.move_y
+        screen.blit(flask.img, (flask.x, flask.y))
+        screen.blit(textsurface,(flask.x + 35, flask.y + 40))
+  
+def click(mouse, flasks):
+    for flask in flasks:
+        if (flask.x + 80 > mouse[0] > flask.x) and\
+        (flask.y + 80  > mouse[1] > flask.y):
+            flasks.remove(flask)
+            flasks.append(Flask())
+            global score, step
+            score += 1
+            step += 1
+            print(step)
+            break
 
-def Game():
-    fairyTale()
-    difficulty = setDifficulty()
-    numbers = genNumbers(difficulty)
-    # change easy to different
-    tasks = {"ulam": (isUlam, "число Улама"), "lucky": (
-        isLucky, "вдале число"), "prime": (isPrime, "просте чило")}
-    counter, end, total, goal = 1, 15, 0, 10
-    while counter < end + 1 and total < goal:
-        current_task = genTask()
-        reward = playLevel(counter, tasks[current_task], numbers, difficulty)
-        total += reward
-        print("\nЦя битва принесла вам профіт у", reward,
-              'балів. Ваша загальна кількість балів становить', total, end="\n\n\n")
-        counter += 1
-    if counter == end+1:
-        print("Ви не впоралися із заданням: Землю захоплено")
-    elif goal == total:
-        print("Ви впоралися із заданням: Землю врятовано!")
+screen = pygame.display.set_mode((1000, 500))
+myfont = pygame.font.SysFont('Comic Sans MS', 20)
+white = (255, 255, 255)
+done = False
+flask_lst = []
+col_flasks = 10
+score = 0
+step = 0
+for i in range(col_flasks):
+    flask_lst.append(Flask())
 
-
-if __name__ == "__main__":
-    Game()
+clock = pygame.time.Clock()
+while not done:
+        if (step == 5):
+            screen.fill(white)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+        else:
+            mouse = (-1, -1)
+            for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                            done = True
+                    if event.type == pygame.MOUSEBUTTONUP:
+                      click(pygame.mouse.get_pos(), flask_lst)
+        
+            screen.fill(white)
+            move(flask_lst)      
+        pygame.display.flip()
+        clock.tick(60)
