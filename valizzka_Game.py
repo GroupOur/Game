4507f5236265245aab53e658e185f0a475c13de5
Centurion256 <Vkuchynskyy@gmail.com>
2018-10-22 22:54:48 +0300

    Translated some text into English

--- valizzka_Game.py
+++ valizzka_Game.py
@@ -6,20 +6,20 @@ def Game():
     difficulty = setDifficulty()
     numbers = genNumbers(difficulty)
     # change easy to different
-    tasks = {"ulam": (isUlam, "число Улама"), "lucky": (
-        None, "вдале число"), "prime": (isPrime, "просте чило")}
+    tasks = {"ulam": (isUlam, "Ulam number"), "lucky": (
+        None, "lucky number"), "prime": (isPrime, "prime number")}
     counter, end, total, goal = 1, 15, 0, 10
     while counter < end + 1 and total < goal:
         current_task = genTask()
         reward = playLevel(counter, tasks[current_task], numbers, difficulty)
         total += reward
-        print("\nЦя битва принесла вам профіт у", reward,
-              'балів. Ваша загальна кількість балів становить', total, end="\n\n\n")
+        print("\nYou scored %i points" % reward,
+              'Your total score is', total, end="\n\n\n")
         counter += 1
     if counter == end+1:
-        print("Ви не впоралися із заданням: Землю захоплено")
+        print("You failed. From this day onwards, no one shall take you seriously")
     elif goal == total:
-        print("Ви впоралися із заданням: Землю врятовано!")
+        print("You succeeded! Now that you have earned yourself a name, more and more people will be intimidated to learn about mathemagic!")
 
 
 if __name__ == "__main__":
